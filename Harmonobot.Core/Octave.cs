7d78152dd381a2b6a8ad6a756de9588d3bfb9729
Janke <Janke@DESKTOP-2EV1M0A>
2022-01-16 00:14:01 +0100

    Add project files.

---
+++ Harmonobot.Core/Octave.cs
@@ -0,0 +1,18 @@
+﻿namespace Harmonobot.Core
+{
+    public enum Octave
+    {
+        None = -1,
+        SubSubContra,
+        SubContra,
+        Contra,
+        Great,
+        Small,
+        OneLine,
+        TwoLine,
+        ThreeLine,
+        FourLine,
+        FiveLine,
+        SixLine
+    }
+}
---
+++ Harmonobot.Core/Pitch.cs
@@ -0,0 +1,19 @@
+﻿namespace Harmonobot.Core
+{
+    public enum Pitch
+    {
+        None = -1,
+        C,
+        CSharp,
+        D,
+        DSharp,
+        E,
+        F,
+        FSharp,
+        G,
+        GSharp,
+        A,
+        ASharp,
+        B
+    }
+}
