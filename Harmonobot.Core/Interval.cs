eefa01dc3524820446dac4d86b345b3dcbaef3cb
janex-PL <kliszczjan@gmail.com>
2022-01-16 23:09:33 +0100

    Added interval enums

---
+++ Harmonobot.Core/Interval.cs
@@ -0,0 +1,32 @@
+﻿namespace Harmonobot.Core
+{
+    public enum Interval
+    {
+        None = -1,
+        PerfectUnison,
+        MinorSecond,
+        MajorSecond,
+        MinorThird,
+        MajorThird,
+        PerfectFourth,
+        Tritone,
+        PerfectFifth,
+        MinorSixth,
+        MajorSixth,
+        MinorSeventh,
+        MajorSeventh,
+        PerfectOctave,
+        MinorNinth,
+        MajorNinth,
+        MinorTenth,
+        MajorTenth,
+        PerfectEleventh,
+        DoubleTritone,
+        PerfectTwelth,
+        MinorThirteenth,
+        MajorThirteenth,
+        MinorFourtenth,
+        MajorFourteenth,
+        PerfectFiftenth
+    }
+}
