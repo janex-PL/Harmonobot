6810760fd7b5d60e6d91ae30a8da8e541412f33b
janex-PL <kliszczjan@gmail.com>
2022-01-19 23:42:52 +0100

    Added ChordType enums, updated Note class to include all names as properties

--- Harmonobot.Core/Note.cs
+++ Harmonobot.Core/Note.cs
@@ -5,9 +5,9 @@
         public Pitch Pitch { get; private set; }
         public Octave Octave { get; private set; }
         public byte Value { get; private set; }
-
-        public string ScientificName => NoteDictionary.GetNoteScientificName(Pitch, Octave);
-        public string HelmholtzName => NoteDictionary.GetNoteHelmholtzName(Pitch, Octave);
+        public string ScientificName { get; private set; } = string.Empty;
+        public string HelmholtzName { get; private set; } = string.Empty;
+        public string PitchName { get; private set; } = string.Empty;
 
         public Note()
         {
@@ -36,6 +36,7 @@
             Value = noteValue;
             Pitch = (Pitch)(Value % pitchCount);
             Octave = (Octave)(Value / octaveCount);
+            UpdateNames();
         }
 
         public void SetValue(Pitch pitch, Octave octave)
@@ -53,6 +54,26 @@
             Pitch = pitch;
             Octave = octave;
             Value = (byte)((byte)octave * MusicConstants.PitchCount + pitch);
+            UpdateNames();
+        }
+
+        private void UpdateNames()
+        {
+            if (Pitch == Pitch.None)
+            {
+                throw new InvalidOperationException(
+                    $"Property '{nameof(Pitch)}' must be different than default value");
+            }
+
+            if (Octave == Octave.None)
+            {
+                throw new InvalidOperationException(
+                    $"Property '{nameof(Octave)}' must be different than default value");
+            }
+
+            PitchName = NoteDictionary.GetPitchName(Pitch);
+            ScientificName = NoteDictionary.GetNoteScientificName(Pitch, Octave);
+            HelmholtzName = NoteDictionary.GetNoteHelmholtzName(Pitch, Octave);
         }
 
     }
